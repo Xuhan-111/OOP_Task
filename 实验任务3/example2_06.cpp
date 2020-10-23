72399e3ac5f9492d1e0896ad99f482814ff96d76
Xuhan-111 <71704172+Xuhan-111@users.noreply.github.com>
2020-10-23 20:14:51 +0800

    Update example2_06.cpp

--- 实验任务3/example2_06.cpp
+++ 实验任务3/example2_06.cpp
@@ -1,45 +1,48 @@
 //example2_06.cpp：域解析符使全局变量真正具有全局作用域
 #include <iostream>
 #include <iomanip>
-using namespace std;                   		                    
+using namespace std;                   //使用C++的标准名字空间
+int sum = 5000; 		                 //定义全局变量sum      
 int main()
 {
-	int sum = 5000;
+	int sum = 200;
 	int arr[3] = { 10,20,30 };
-	{                               
-		int i, sum = 0;                    
+	{                                
+		int i, sum = 0;                                  
 		for (i = 0; i < 3; i++)
-			sum += arr[i];                  
-		cout << "sum=" << sum << endl;      
-		sum += sum;	                   
-		cout << "全局sum=" << sum << endl;  
+			sum += arr[i];                 
+		cout << "sum=" << sum << endl;       
+		::sum += sum;	                   
+		cout << "全局sum=" << ::sum << endl;  
 	}                                
-	sum *= 2;                           
-	cout << "sum=" << sum << endl;           
-	sum = 200;   //直接对sum进行赋值，而非定义，否则报错为重定义                    
-	cout << "sum=" << sum << endl;           
-	sum += sum;           	          
-	cout << "全局sum=" << sum << endl;   
+	sum *= 2;                            
+	cout << "sum=" << sum << endl;                                   	
+	cout << "sum=" << sum << endl;          
+	::sum += sum;           	          	
+	cout << "全局sum=" << ::sum << endl;     
 	return 0;
 }
 /*
-原运行结果：
+原程序运行结果:
 sum=60
 全局sum=5060
 sum=10120
 sum=200
 全局sum=10320
-思考与练习中运行结果：将原程序19行的定义赋值改为赋值
+修改后程序运行结果:
 sum=60
-全局sum=120
-sum=10000
-sum=200
-全局sum=400
+全局sum=5060
+sum=400
+sum=400
+全局sum=5460
 */
 /*
-删除sum前的域解析符，并将19行的定义改为赋值后方可运行程序，因原19行的sum
-与函数开头定义的sum在同一程序块中，无法定义两个相同名称的变量。
-输出第一个sum是程序块中定义的sum=10+20+30=60。
-第一个全局sum也为程序块中定义的sum=60+60=120。
-出程序块后，所有sum均为函数开头定义的sum
+程序分析：
+调整后sum=200是第一个被定义的局部变量。在小程序块中定义了第二个局部变量
+sum。因此第一个输出语句输出的是对arr数组中数据求和的结果，为60。由于使用了
+域解析符，第二个输出语句输出的是修改后的全局变量sum值，为5060。
+小程序块结束，第二个全局变量sum作用结束。之后的sum*=2是对局部变量sum的
+修改，之后的两个输出语句输出的均为局部变量sum的值，为400。::sum+=sum;
+语句中，左边的sum显然是全局变量sum，右边则为局部变量sum，即为5060+400=5460。
+最后一条输出语句输出的是全局变量sum的值，为5460。
 */
